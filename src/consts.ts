997b59b7a0353836f90b1d6a96dd77c2c287b9db
Karl Weinmüller <karlweinmueller@me.com>
2024-11-16 21:35:08 +0100

    change basic settings

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const BRAND = "Acme";
-export const SITE_TITLE = "Astro Engineering Blog";
-export const SITE_URL = "https://astro-engineering-blog.netlify.app";
-export const SITE_DESCRIPTION = "Welcome to our Engineering blog!";
+export const BRAND = 'KW';
+export const SITE_TITLE = 'KW';
+export const SITE_URL = 'https://astro.karlweinmueller.de/';
+export const SITE_DESCRIPTION = 'Willkommen auf meiner Webseite';
 export const PAGINATION_POSTS_PER_PAGE = 5;
